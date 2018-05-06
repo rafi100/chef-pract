c95561b6ca75d0b725980af966058533007beccb
rafi3 <rafi3linux@gmail.com>
2018-05-06 17:00:09 +0530

    add chef repo

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd' do 
+	action :install
+end
+
+service 'httpd' do
+	action [:enable, :start]
+end
